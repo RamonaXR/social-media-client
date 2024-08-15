5bddcd12c47d968fb01b2e466f37f30f1d07d0d4
RamonaXR <Ramonaelise@gmail.com>
2024-08-15 15:23:34 +0200

    added cypress login test

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,32 @@
+describe('Valid Login Tests', () => {
+  beforeEach(() => {
+    cy.fixture('credentials.json').as('credentials');
+    cy.visit('/login');
+  });
+
+  it('logs in with valid credentials and stores token and profile in localStorage', function () {
+    cy.intercept('POST', 'https://nf-api.onrender.com/api/v1/social/auth/login').as('loginRequest');
+
+    cy.get('[data-cy="emailInput"]').click();
+    cy.get('@credentials').then((user) => {
+      cy.get('[data-cy="emailInput"]').type(user.validEmail);
+      cy.get('[data-cy="passwordInput"]').click();
+      cy.get('[data-cy="passwordInput"]').type(user.validPassword);
+    });
+
+    cy.get('[data-cy="login-btn"]').click();
+
+    cy.wait('@loginRequest').then((interception) => {
+      expect(interception.response.statusCode).to.eq(200);
+
+      // Verify localStorage
+      cy.window().then((win) => {
+        expect(win.localStorage.getItem('token')).to.exist;
+        expect(win.localStorage.getItem('profile')).to.exist;
+      });
+
+      // Verify the URL
+      cy.location('pathname').should('eq', '/');
+    });
+  });
+});
